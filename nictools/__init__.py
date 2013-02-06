c32fcaf2ca6ef728d8ee64dc90f0ad64f27a4d5c
sienkiew <sienkiew@stsci.edu>
2013-02-06 20:27:51 +0000

    get version from new install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/nictools/trunk@22258 fe389314-cf27-0410-b35b-8c050e845b92

--- nictools/__init__.py
+++ nictools/__init__.py
@@ -17,11 +17,5 @@ Utility and library functions used by these tasks are also included in this
 module.
 """
 
-try:
-    from .version import (__version__, __svn_revision__,
-                          __svn_full_info__, __setup_datetime__)
-except ImportError:
-    __version__ = ''
-    __svn_revision__ = ''
-    __svn_full_info__ = ''
-    __setup_datetime__ = None
+from .version import * 
+
