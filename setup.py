b64d6fc5bbdf001d4b7a01af9f5f8e4a83f8208c
sienkiew <sienkiew@stsci.edu>
2013-07-23 19:36:37 +0000

    bringing release changes back to trunk
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/nictools/trunk@25916 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.dev'],
+    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.2'],
     d2to1=True,
     use_2to3=True,
     zip_safe=False
