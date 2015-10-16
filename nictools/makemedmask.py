pllim/nictools

commit b5aeb9b650f1e87b8ac69c1a618c1789059810f7

Replace pyfits with astropy.io.fits
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/nictools/trunk@45449 fe389314-cf27-0410-b35b-8c050e845b92

nictools/makemedmask.py
@@ -7,7 +7,7 @@
 from __future__ import absolute_import, division, print_function  # confidence high
 
 import numpy as np
-import pyfits, sys, string, time
+from astropy.io import fits as pyfits, sys, string, time
 from stsci.convolve import boxcar
 from optparse import OptionParser
 from . import fsutil, opusutil

commit f0b92e84edd0f7464eceb7030de1304a3b3b11c8

Fixed import error introduced by r45449. Removed unnecessary string import.
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/nictools/trunk@45578 fe389314-cf27-0410-b35b-8c050e845b92

nictools/makemedmask.py
@@ -6,8 +6,10 @@
 # History: 03/04/08 - first version
 from __future__ import absolute_import, division, print_function  # confidence high
 
+import sys
+import time
 import numpy as np
-from astropy.io import fits as pyfits, sys, string, time
+from astropy.io import fits as pyfits
 from stsci.convolve import boxcar
 from optparse import OptionParser
 from . import fsutil, opusutil
@@ -51,7 +53,7 @@ class Makemedmask:
         if (medfile == None):  medfile = fsutil.medfile
         if (callist == None):  callist = fsutil.callist
         if (type(thresh) == str) : # ensure that thresh is a float
-           thresh = string.atof(thresh)
+           thresh = float(thresh)
 
         self.thresh = thresh
         self.medfile = medfile